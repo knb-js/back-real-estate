150423cfc9b59a947e4e3ff99f48cf362148d480
knunez <kah.nunez@duocuc.cl>
2024-03-29 03:00:40 -0300

    KN - Se agrega API-REST Cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'bwcuj4qdqel5jkco6uwp-mysql.services.clever-cloud.com',
+      port: 3306,
+      username: 'u1yg4oy7upkajc3n',
+      password: 'Rq58mbodx9VZoT9aUglD',
+      database: 'bwcuj4qdqel5jkco6uwp',
+      entities: [],
+      synchronize: true,
+    }),
+    
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
